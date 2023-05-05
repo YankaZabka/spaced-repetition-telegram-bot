5310f0ff23d7b82bc32bd6a8becacd186982787b
yankazabka <yanka.zabka@gmail.com>
2023-05-05 13:34:07 +0200

    fix(duck): refactor @dayjs and @scheduler setups #2

--- src/duck/index.ts
+++ src/duck/index.ts
@@ -3,3 +3,6 @@ import * as types from './types.js';
 import * as utils from './utils.js';
 
 export { constants, types, utils };
+export { default as dayjs } from "./@dayjs/index.js";
+export { default as scheduler } from "./@toad-sheduler/index.js";
+
